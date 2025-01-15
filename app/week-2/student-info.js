14e37c63ce1fc673bc677427bad220966ce46efb
flammabl3 <harryjung135@gmail.com>
2025-01-15 14:24:08 -0700

    Week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo () {
+    return (
+        <div>
+            <h1>Harry Jung</h1>
+            <Link href="https://github.com/flammabl3/">Github</Link>
+        </div>
+    );
+}
